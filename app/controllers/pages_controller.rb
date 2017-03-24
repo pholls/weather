23e6c0e936aeacb7aea709f0149c4020095d9f05
Pat Holland <pholls@users.noreply.github.com>
2017-03-24 13:53:38 -0700

    break display into separate view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
   def index
-
+    render :index
   end
 
   def search
     # make the API call
+    # save to DB?
+    render :display
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#index'
+
+  get '/search', to: 'pages#search'
 end
